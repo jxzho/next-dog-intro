44433d3e1a858bd22bb42735b4ca4737d5b23e9c
russell <879917302@qq.com>
2024-09-28 17:50:14 +0800

    feat: compact vercel read build file with `path.join`

--- app/resume/sync/server.tsx
+++ app/resume/sync/server.tsx
@@ -1,20 +1,19 @@
 'use server'
 
 import fs from 'fs/promises'
+import path from 'path'
+
+const htmlStaticPath = path.join(process.cwd(), 'app/resume/sync/html_static_resume')
 
 export async function updateToServer (html: string) {
-  return await fs.writeFile(
-    process.cwd() + '/app/resume/sync/html_static_resume', 
-    html,
-    {
-      encoding: 'utf8'
-    }
-  )
+  return await fs.writeFile(htmlStaticPath, html, {
+    encoding: 'utf8'
+  })
 }
 
 export async function getFromServer () {
   return await fs.readFile(
-    process.cwd() + '/app/resume/sync/html_static_resume',
+    htmlStaticPath,
     {
       encoding: 'utf8'
     }
